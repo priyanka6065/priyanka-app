9b5508d2cfb5b348c83ca06278b419214e82b443
nutan <nutanusadadiya@gmail.com>
2020-08-03 18:08:03 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { AwardComponent } from './award/award.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { InterestsComponent } from './interests/interests.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+const appRoutes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'about-me', component: AboutComponent },
+  { path: 'award', component: AwardComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'interests', component: InterestsComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'skills', component: SkillsComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    AwardComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    InterestsComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
